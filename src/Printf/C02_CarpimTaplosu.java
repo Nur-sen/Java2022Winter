cccb10fd4b88cf3ba47f92dd798571589a3dadeb
Nur-sen <nursenalkan84@gmail.com>
2022-04-21 01:19:09 +0300

    21.04.2022

---
+++ src/Printf/C02_CarpimTaplosu.java
@@ -0,0 +1,24 @@
+package Printf;
+
+public class C02_CarpimTaplosu {
+    public static void main(String[] args) {
+       /*
+        int input=5;
+        output: 1  2  3  4 5
+                2  4  6  8 10
+                3  6  9 12 15
+                4  8 12 16 20
+                5 10 15 20 25
+        */
+        int input=10;
+        for (int i = 1; i <input; i++) {
+            for (int j = 1; j <input ; j++) {
+
+                System.out.printf("%4d",i*j );
+
+            }
+            System.out.println("");
+
+        }
+    }
+}
